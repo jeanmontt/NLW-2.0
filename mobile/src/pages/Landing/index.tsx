c5faa37aa8e7bb0167c3080e3a610bea46f40bc2
Jean Monteiro <jeanmontt@hotmail.com>
2020-08-10 16:47:30 -0300

    Next Level Week finished

--- mobile/src/pages/Landing/index.tsx
+++ mobile/src/pages/Landing/index.tsx
@@ -1,6 +1,9 @@
-import React from 'react';
-import { View, Image, Text, TouchableOpacity } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, Image, Text } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
+import { RectButton } from 'react-native-gesture-handler';
+
+import api from '../../services/api';
 
 import styles from './styles';
 
@@ -11,9 +14,22 @@ import heartIcon from '../../assets/images/icons/heart.png';
 
 function Landing() {
     const { navigate } = useNavigation();
+    const [totalConnections, setTotalConnections] = useState(0); 
+
+    useEffect(() => {
+        api.get('/connections').then(response => {
+            const { total } = response.data;
+
+            setTotalConnections(total);
+        })
+    }, []);
 
     function handleNavigateToGiveClassesPage() {
         navigate('GiveClasses');
+    }
+
+    function handleNavigateToStudyPages() {
+        navigate('Study');
     }
 
     return (
@@ -28,24 +44,27 @@ function Landing() {
             </Text>
 
             <View style={styles.buttonsContainer}>
-                <TouchableOpacity style={[styles.button, styles.buttonPrimary]}>
-                    <Image source={studyIcon} />
+                <RectButton 
+                    onPress={handleNavigateToStudyPages} 
+                    style={[styles.button, styles.buttonPrimary]}
+                >
+                        <Image source={studyIcon} />
 
-                    <Text style={styles.buttonText}>Estudar</Text> 
-                </TouchableOpacity>
+                        <Text style={styles.buttonText}>Estudar</Text> 
+                </RectButton>
 
-                <TouchableOpacity 
+                <RectButton 
                     onPress={handleNavigateToGiveClassesPage} 
                     style={[styles.button, styles.buttonSecondary]}
                 >
-                    <Image source={giveClassesIcon} />
+                        <Image source={giveClassesIcon} />
 
-                    <Text style={styles.buttonText}>Dar aulas</Text> 
-                </TouchableOpacity>
+                        <Text style={styles.buttonText}>Dar aulas</Text> 
+                </RectButton>
             </View>
 
             <Text style={styles.totalConnections}>
-                Total de 285 conexões já realizadas {' '}
+                Total de {totalConnections} conexões já realizadas {' '}
                 <Image source={heartIcon} />
             </Text>
         </View>
